f7deb897d3fd611d5e9f592c5f775bb16c8db0ec
Augusto Silva <augusto.silva@outlook.com>
2024-03-19 19:07:18 -0300

    Alteração no desafio do CPF

--- Consulta_CPF/Consulta_CPF.rb
+++ Consulta_CPF/Consulta_CPF.rb
@@ -1,5 +1,28 @@
 require "cpf_cnpj"
 
+=begin 
+
+Esse Codigo é o feito pela professora: ESSE CODIGO ESTA EXCLUINDO O 0 INICIAL DO CPF
+
+def check_cpf (cpf_number)
+  if CPF.valid?(cpf_number)
+    return "O CPF informado é valido"
+  else
+    return "O CPF informado é invalido"
+  end
+end
+
+print 'Digite seu CPF: '
+cpf_number = gets.chomp.to_i
+puts cpf_number
+
+result = check_cpf(cpf_number)
+
+puts result
+
+
+=end
+
 print 'Por favor, digite o CPF contendo apenas numeros: '
 
 cpf = gets.chomp
